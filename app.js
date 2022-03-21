1bebec3b30dee67f1b6263d218f94f1f255c1f69
martin-creator <martinlubowa@outlook.com>
2022-03-21 23:41:23 +0300

    first route built

---
+++ app.js
@@ -0,0 +1,23 @@
+/**
+ *Require Libraries
+*/
+
+const express = require('express');
+
+// App Setup
+
+const app = express ();
+
+//Middleware
+
+//Routes
+
+app.get('/', (req,res) => {
+	res.send("Hello Squirrel");
+});
+
+//Start Server
+
+app.listen(3000, () => {
+	console.log('Gif Search is listening on port localhost :3000!');
+});
